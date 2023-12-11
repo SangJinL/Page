6a0fb5ff069046b8b7c39776d6c05de29083e16e
LeeSangJin <kyun212k@gmail.com>
2023-12-12 03:37:22 +0900

    Changes
    일단 login, signup 반 완료

--- src/main/java/test/SecurityConfiguration.java
+++ src/main/java/test/SecurityConfiguration.java
@@ -6,17 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers("/", "/home", "/signup").permitAll() // 특정 경로에 대한 접근 허용
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/", "/main", "/signup").permitAll() // 특정 경로에 대한 접근 허용
                                 .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
                 )
                 .formLogin(formLogin ->
@@ -25,7 +24,7 @@ public class SecurityConfig {
                                 .permitAll()
                 )
                 .logout(logout ->
-                        logout.permitAll()
+                        logout.permitAll() // 로그아웃에 대한 모든 요청 허용
                 );
 
         return http.build();
--- src/main/java/test/WebSecurityConfigurerAdapter.java
+++ src/main/java/test/WebSecurityConfigurerAdapter.java
@@ -2,7 +2,6 @@ package test;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-
 public abstract class WebSecurityConfigurerAdapter {
     protected abstract void configure(HttpSecurity http) throws Exception;
 }
